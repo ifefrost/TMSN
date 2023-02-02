bb5379689efe017682ee364b622fb93ef19d1bf6
Kwinnie <kwinniemaefianza@gmail.com>
2023-02-02 13:29:43 -0500

    Update Search.jsx
    fixed search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,18 +2,19 @@ import {MdOutlineSearch} from 'react-icons/md';
 
 const Search = () => {
     return (
-        <div>
+    <div>
+        <div className="mx-auto 2xl:max-w-screen-xl px-8">
 
-        <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-        <span class="sr-only">Open sidebar</span>
-            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-            </svg>
-        </button>
+            <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <span className="sr-only">Open sidebar</span>
+                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                </svg>
+            </button>
 
-        <aside id="default-sidebar" class="fixed top-20 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-32" aria-label="Sidebar">
-            <div class="h-full px-3 py-4 overflow-y-auto bg-slate-900 dark:bg-slate-900 rounded-md">
-                <ul class="space-y-2">
+        <aside id="default-sidebar" className="fixed top-20 -mr-80 left-0 z-40 w-64 h-fit transition-transform -translate-x-full sm:translate-x-20" aria-label="Sidebar">
+            <div className="h-full px-3 py-4 overflow-y-auto bg-slate-900 dark:bg-slate-900 rounded-md">
+                <ul className="space-y-2">
                     <li>
                         <span className="ml-5 font-bold text-lg text-gray-50">Filter by</span>
                     </li>
@@ -46,7 +47,7 @@ const Search = () => {
             </div>
         </aside>
 
-        <div class="container mx-auto">
+        <div className="container mx-auto">
 
             <div className="p-5 sm:ml-80">
                 <div className="p-2">
@@ -66,24 +67,24 @@ const Search = () => {
                 </div>
             </div>
 
-            <div class="p-5 sm:ml-80">
-                <div class="px-10">
-                    <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
-                        <div class="flex items-center justify-center h-[340px] w-[226px] rounded bg-gray-50 dark:bg-gray-800 drop-shadow-lg">
-                            <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-2xl text-dark font-semibold">Movie</div>
+            <div className="p-5 sm:ml-80">
+                <div className="px-10">
+                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
+                        <div className="flex items-center justify-center h-[340px] w-[226px] rounded bg-gray-50 dark:bg-gray-800 drop-shadow-lg">
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-2xl text-dark font-semibold">Movie</div>
                         </div>
-                        <div class="flex items-center justify-center h-[340px] w-[226px]  rounded bg-gray-50 dark:bg-gray-800 drop-shadow-lg">
-                            <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-2xl text-dark font-semibold">Movie</div>
+                        <div className="flex items-center justify-center h-[340px] w-[226px]  rounded bg-gray-50 dark:bg-gray-800 drop-shadow-lg">
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-2xl text-dark font-semibold">Movie</div>
                         </div>
-                        <div class="flex items-center justify-center h-[340px] w-[226px]  rounded bg-gray-50 dark:bg-gray-800 drop-shadow-lg">
-                            <div class="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-2xl text-dark font-semibold">Movie</div>
+                        <div className="flex items-center justify-center h-[340px] w-[226px]  rounded bg-gray-50 dark:bg-gray-800 drop-shadow-lg">
+                            <div className="opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center items-center text-2xl text-dark font-semibold">Movie</div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
+</div>
     )
 }
 
