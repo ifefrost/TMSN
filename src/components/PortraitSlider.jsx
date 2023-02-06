2f29c5c50c9e047a3d1c702a6f63ccb3c2320f88
Collin <collinkavaner@gmail.com>
2023-02-06 17:15:12 -0500

    small adjustment to movie/tv page

--- src/components/PortraitSlider.jsx
+++ src/components/PortraitSlider.jsx
@@ -29,8 +29,8 @@ const PortraitSliderItem = ({ movie }) => {
           className="rounded-xl object-cover h-[250px] w-[165px]"
       />
       <div className="w-[165px]">
-          <p className="text-white font-bold text-[1.125rem]">{movie.title}</p>
-          <p className="text-white text-[1rem]">{movie.date}</p>
+          <p className="mt-3 text-white font-bold text-[1.125rem]">{movie.title}</p>
+          <p className="text-[#AAA9A9] text-[1rem]">{movie.date}</p>
       </div>
     </div>
   );
