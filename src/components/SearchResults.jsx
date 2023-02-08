d920ecb73fb9eb736dc9a41724536c7950ffb114
Collin <collinkavaner@gmail.com>
2023-02-07 19:58:59 -0500

    more styling on overall website

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -4,8 +4,8 @@ const SearchResults = ({ results }) => {
   return (
     <div className="flex flex-wrap mt-10 gap-5 w-[760px]">
       {results.length === 0 && (
-        <div className="flex justify-content">
-        <h1 className="text-lg text-white">No Results</h1>
+        <div>
+          <h1 className="text-lg text-white">No Search Results.</h1>
         </div>
       )}
       {results.map((result) => (
