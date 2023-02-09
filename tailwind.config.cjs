5eb15cbcf3742e34312ef7e32ecde9cd91320f70
Collin <collinkavaner@gmail.com>
2023-02-09 14:22:52 -0500

    added hero and footer images to the home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,10 @@ module.exports = {
       boxShadow: {
         DEFAULT: "0px 0px 21px 0px rgba(144, 163, 232, 0.32)",
       },
+      backgroundImage: {
+        "hero-image" : "linear-gradient(to right, rgba(13, 37, 63, 0.8) 0%, rgba(1, 180, 228, 0.5) 100%), url('https://image.tmdb.org/t/p/original/s16H6tpK2utvwDtzZ8Qy4qm5Emw.jpg')",
+        "footer-image": "linear-gradient(to right, rgba(13, 37, 63, 1) 0%, rgba(1, 180, 228, 0.5) 100%), url('https://image.tmdb.org/t/p/original/ncC9ZgZuKOdaVm7yXinUn26Qyok.jpg')",
+      },
       fontWeight: {
         thin: 100,
         extralight: 200,
