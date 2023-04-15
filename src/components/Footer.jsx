2afb14590570fe32a0afbf9c607a71e776038d0f
Collin <collinkavaner@gmail.com>
2023-04-15 11:58:32 -0400

    fixed the footer so all links and styles work

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,37 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const token = sessionStorage.getItem("token");
+  const username = sessionStorage.getItem("user");
   const navigate = useNavigate();
   return (
     <footer className='bg-[#1F2230] py-10 text-white text-center w-full mt-auto'>
       <div className='mx-auto 2xl:max-w-screen-xl py-10 px-2 sm:px-6 lg:px-8 flex flex-col items-center'>
         <div className='flex items-center '>
           <div className='pr-3'>
-            <h1 className='text-4xl mb-3 no-underline text-white font-sans font-bold text-right'>
+            <h1 className='text-4xl mb-1 no-underline text-white font-sans font-bold text-right'>
               TMSN
             </h1>
-            <button className='bg-blue-700 h-[46px] border-white border-2 hover:bg-blue-900 text-white font-bold py-1 px-4 mt-3 rounded-full hover:shadow' onClick={() => navigate('/register')}>
+            <button className='bg-blue-700 h-[46px] border-white border-2 hover:bg-blue-900 text-white font-bold py-1 px-4 mt-1 rounded-full hover:shadow' onClick={() => navigate('/register')}>
               Join the community
             </button>
           </div>
           <ul className='flex flex-col gap-2 text-left pl-3'>
             <li className='mx-3'>
-              <a href=''>Movies</a>
+              <Link to='/' className="hover:underline">Home</Link>
             </li>
             <li className='mx-3'>
-              <a href=''>TV Shows</a>
+              <Link to='/Search' className="hover:underline">Search</Link>
             </li>
             <li className='mx-3'>
-              <a href=''>Search</a>
-            </li>
-            <li className='mx-3'>
-              <a href=''>Account</a>
+              {/* if logged in go to profile and reload page*/}
+              {token ? (
+                <Link to={`/${username}`} className="hover:underline">
+                  Profile
+                </Link>
+              ) : (
+                <Link to='/login' className="hover:underline">Login</Link>
+              )}
             </li>
           </ul>
         </div>
